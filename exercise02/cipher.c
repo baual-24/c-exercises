5d53a5796a783796f00effffa13e2fc8c15d2dce
Amirah Bauder <baual-24@rhodes.edu>
2023-02-22 20:42:02 -0600

    Kata2 assignment completed

--- exercise02/cipher.c
+++ exercise02/cipher.c
@@ -1,60 +1,49 @@
+#include <ctype.h>  // for isspace()
 #include <stdio.h>  // for printf() and scanf()
 #include <string.h> // for various string functions
 
 int main(int argc, char **argv) {
-  // char sub[26];
-  // printf("Enter substitution pad: ");
-  // scanf("%s", sub);
-
-  // char input[1026];
-  // scanf("%s", input);
 
   char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
-  // char shortAlph[3] = "abc";
 
-  // char encodedInput[1026];
-
-  char sub[26] = "qwertyuiopasdfghjklzxcvbnm";
-  printf("Enter substitution pad: qwertyuiopasdfghjklzxcvbnm\n");
+  char sub[26];
+  printf("Enter substitution pad: ");
+  scanf("%s", sub);
 
   char input[1024];
-  scanf("%s", input);
+  char encodedInput[1024];
 
-  // 1) we're comparing the letter at input[i] (ex: input[0] = 'h')
-  // 2) check where that letter is in the location of alphabet. (ex: h  =
-  // alphabet[7]). 3) find the letter that's at that index in sub (ex: sub[7] =
-  // 'i'). 4) add that letter to editedInput
-
-  char letter = 'h';
-  char *indxptr = strchr(alphabet, 'h');
-  int index = indxptr - alphabet;
-  printf("Letter: %c\n", letter);
-  printf("Index in alphabet (should be 7): %d\n", index);
-
-  /*  for (int i = 0; i < 26; i++) {
-      if (sub[i] >= 'a') { // if character at position 'i' is a letter "greater
-        // printf("%c is greater than a", sub[0]);
-      }
+  // Encoding the inputted string:
+  while (1) {
+    // Clear input
+    for (int i = 0; i < 1024; i++) {
+      input[i] = '\0';
     }
-  */
 
-  // Caveman approach:
-  /*  for (int i = 0; i < strlen(input); i++) {
-      if (input[i] == 'a') {
-        input[i] = 'q';
-      } else if (input[i] == 'b') {
-        input[i] = 'w';
-      } else if (input[i] == 'c') {
-        input[i] = 'e';
-      } else if (input[i] == 'd') {
-        input[i] = 'r';
-      } else if (input[i] == 'e') {
-        input[i] = 't';
+    fgets(input, 1024, stdin);
+    input[strlen(input) - 1] = '\0';
+
+    // Clearing encodedInput
+    memset(encodedInput, '\0', 1024);
+
+    // Encoding the input:
+    char letter;
+    char *indxptr; // index pointer.
+    int index;     // index of the given letter in the alphabet.
+    char encodedLetter;
+    for (int i = 0; i < strlen(input); i++) {
+      letter = input[i];
+
+      if (islower(letter) != 0) {
+        indxptr = strchr(alphabet, letter);
+        index = indxptr - alphabet;
+        encodedLetter = sub[index];
+        encodedInput[i] = encodedLetter;
       } else {
-        input[i] = 'O';
+        encodedInput[i] = letter;
       }
     }
-  */
 
-  printf("%s", input);
+    printf("%s\n", encodedInput);
+  }
 }
