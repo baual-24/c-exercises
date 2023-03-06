8a0a3fdecf555110f4cda17710f7c8fb360411fb
Amirah Bauder <baual-24@rhodes.edu>
2023-03-06 15:51:11 -0600

    Final draft of Kata4

--- exercise04/reverse.c
+++ exercise04/reverse.c
@@ -7,11 +7,19 @@ int main(int argc, char **argv) {
   // Initializing variables:
   int *input_arr = NULL; // pointer to array to populate.
   int arr_size = 0;      // current size of array.
-  int num_read = 0;
-  char input[8];
+  int num_read = 0;      // how many inputs have been read thus far.
+  char input[13]; // Size is 13 to account for the 10 digits that could be in
+                  // the largest unsigned int, 1 for the possibility of a
+                  // minus sign, 1 for the null terminator, and 1 for the
+                  // newline character at the end (a result of using fgets)..
 
-  while (num_read != 15) {
-    fgets(input, sizeof(input), stdin);
+  while (1) {
+
+    // NOTE: this if-statement simultaneously executes fgets() and evaluates
+    // its return value.
+    if (fgets(input, sizeof(input), stdin) == NULL) {
+      break;
+    }
 
     // Removing the \n at the end of the input
     input[strlen(input) - 1] = '\0';
@@ -24,10 +32,11 @@ int main(int argc, char **argv) {
 
     // Add input to the end of the input array.
     input_arr[num_read] = atoi(input);
+
     num_read++; // I.e. user typed another input.
   }
 
-  // Print out the reversed inputs:
+  // Print out the reversed inputs in a line:
   for (int i = num_read - 1; i >= 0; i--) {
     printf("%d ", input_arr[i]);
   }
